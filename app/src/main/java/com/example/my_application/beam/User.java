b7e8d94ac6ced617c3cda71d32913ec3b28f3f94
Zongze Yu <a1817713@adelaide.edu.au>
2022-08-18 23:01:39 +0930

    Register page bug fixed

--- app/src/main/java/com/example/my_application/beam/User.java
+++ app/src/main/java/com/example/my_application/beam/User.java
@@ -38,5 +38,13 @@ public class User {
         this.icon = icon;
     }
 
-
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", icon=" + icon +
+                '}';
+    }
 }
