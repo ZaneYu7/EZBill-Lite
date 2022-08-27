46ec04f064116ee9b39f954528c5f0f38769f336
Zongze Yu <a1817713@adelaide.edu.au>
2022-08-28 00:28:21 +0930

    initiate Firebase and finish the register function

--- app/src/main/java/com/example/my_application/beam/User.java
+++ app/src/main/java/com/example/my_application/beam/User.java
@@ -1,50 +1,16 @@
 package com.example.my_application.beam;
 
 public class User {
-    private int id;
-    private String username;
-    private String password;
-    private int icon;
 
-    public int getId() {
-        return id;
+    public String fullName, userName, email;
+
+    public User() {
+
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public int getIcon() {
-        return icon;
-    }
-
-    public void setIcon(int icon) {
-        this.icon = icon;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                ", icon=" + icon +
-                '}';
+    public User(String fullName, String userName, String email) {
+        this.fullName = fullName;
+        this.userName = userName;
+        this.email = email;
     }
 }
